9e870650d03697b257e53c8c75f55551d807f953
N-JJ26 <nathanielj613@gmail.com>
2024-11-12 11:39:37 -0500

    added a main()

--- src/Notes/ArrayListNotes.java
+++ src/Notes/ArrayListNotes.java
@@ -1,6 +1,7 @@
 package src.Notes;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListNotes {
     /*
@@ -20,8 +21,8 @@ public class ArrayListNotes {
      * java.util.ArrayList
      */
 
-    static ArrayList<String> arrayList1 = new ArrayList<String>(); //must use objects
-    static ArrayList<Character> arrayList2 = new ArrayList<Character>();
+    //static ArrayList<String> arrayList1 = new ArrayList<String>(); //must use objects
+    //static ArrayList<Character> arrayList2 = new ArrayList<Character>();
 
     /*
      * Boolean
@@ -30,14 +31,44 @@ public class ArrayListNotes {
      * Double
      */
 
+
+    private static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        arrayList1.add("first item");
-        arrayList1.add("second");
-        //... all should return true
-        //adds 5 "lockers" once the size has been filled, starting with 10
+        ArrayList<Character> list = new ArrayList<Character>();
+        //or   List list = new ArrayList<Character>();  on the AP
 
-        arrayList1.toString(); //[0, 1, 2, ...]
-
-        //cannot use set() to an empty array
+        //can be used for the lab
+        while(true) {
+            System.out.println("Menu:\n" +
+                            "(P)rint, is empt(Y), clea(R), (G)et element, (S)et element,\n" +
+                            "(C)ontains an element, i(N)dex of element,\n" +
+                            "(L)ast index of element, (A)dd at the end, add at (I)ndex,\n" +
+                            "re(M)ove at index, remove(E)lement, or e(X)itn\n");
+            char choice = in.nextLine().toLowerCase().charAt(0);
+            switch( choice ) {
+                case 'p':
+                    System.out.println(list.toString());
+                    break;
+                case 's':
+                    System.out.println(list.size());
+                    break;
+                case 'x':
+                    return;
+                case 'y':
+                    System.out.println(list.isEmpty());
+                        //list.isEmpty() => list.size() == 0
+                    break;
+                case 'r':
+                    list.clear(); //void return cannot be in a println()
+                    System.out.println("cleared");
+                    break;
+                case 'g':
+                    int index = in.nextInt();
+                    in.nextLine(); //scrap the <enter>
+                    System.out.println(list.get(index));
+                    break;
+            }
+        }
     }
 }
