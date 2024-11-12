9e5efd3cc8063f96778232ee40ebc4e2e56c73f0
N-JJ26 <nathanielj613@gmail.com>
2024-11-12 11:10:36 -0500

    ArrayList notes

---
+++ src/Notes/ArrayListNotes.java
@@ -0,0 +1,41 @@
+package src.Notes;
+
+import java.util.ArrayList;
+
+public class ArrayListNotes {
+    /*
+     * major drawback of arrays => fixed size
+     *      an array's size larger than necessary
+     *      partially filled array
+     *          store information amount used
+     *
+     *  0   1   2 ...
+     * ____________________
+     *| x | x | x |   |   |
+     * ____________________
+     *
+     *
+     *
+     *
+     * java.util.ArrayList
+     */
+
+    static ArrayList<String> arrayList1 = new ArrayList<String>(); //must use objects
+    static ArrayList<Character> arrayList2 = new ArrayList<Character>();
+
+    /*
+     * Boolean
+     * Character
+     * Integer
+     * Double
+     */
+
+    public static void main(String[] args) {
+        arrayList1.add("first item");
+        arrayList1.add("second");
+        //... all should return true
+        //adds 5 "lockers" once the size has been filled, starting with 10
+
+        arrayList1.toString(); //[0, 1, 2, ...]
+    }
+}
