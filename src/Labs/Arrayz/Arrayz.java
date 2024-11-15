b97380434edfe213de68b583f3e84e42cbe0cdea
N-JJ26 <nathanielj613@gmail.com>
2024-11-15 14:20:49 -0500

    beginning the Arrayz degroat lab

---
+++ src/Labs/Arrayz/Arrayz.java
@@ -0,0 +1,208 @@
+package src.Labs.Arrayz;
+
+/**
+ * JAVADOC HERE
+ *
+ * @author Nate Johnson
+ * @version 11/15/24
+ */
+public class Arrayz
+{
+    /*TODO:
+     * javadoc
+     * remove package header
+     */
+
+    /** JAVADOC */
+    public static final int CAPACITY = 20;
+
+    private static final int INCREMENT = 5;
+
+    private int numberOfElements;
+    private char[] charArray;
+
+    /**
+     * Instantiates the Arrayz object with the correct initial size
+     */
+    public Arrayz()
+    {
+        numberOfElements = 0;
+        charArray = new char[ CAPACITY ];
+        for( int i = 0; i < charArray.length; i++ )
+            charArray[ i ] = ' ';
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @return
+     */
+    public boolean isEmpty()
+    {
+        return numberOfElements == 0;
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @return
+     */
+    public int size()
+    {
+        return numberOfElements;
+    }
+
+    /**
+     * JAVADOC
+     */
+    public void clear()
+    {
+        charArray = new char[ CAPACITY ];
+
+        for( int i = 0; i < charArray.length; i++ )
+            charArray[ i ] = ' ';
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @param index
+     * @return
+     */
+    public char get( int index )
+    {
+        return charArray[ index ];
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @param index
+     * @param element
+     * @return
+     */
+    public char set( int index, char element )
+    {
+        
+        return charArray[ index ];
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @param element
+     * @return
+     */
+    public boolean contains( char element )
+    {
+        for( int i = 0; i < charArray.length; i++ )
+            if( charArray[ i ] == element )
+                return true;
+        return false;
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @param element
+     * @return
+     */
+    public int indexOf( char element )
+    {
+        for( int i = 0; i < charArray.length; i++ )
+            if( charArray[ i ] == element )
+                return i;
+        return -1;
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @param element
+     * @return
+     */
+    public int lastIndexOf( char element )
+    {
+        for( int i = charArray.length - 1; i >= 0; i-- )
+            if( charArray[ i ] == element )
+                return i;
+        return -1;
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @param element
+     * @return
+     */
+    public boolean add( char element )
+    {
+        return false;
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @param index
+     * @param element
+     * @return
+     */
+    public boolean add( int index, char element )
+    {
+        return false;
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @param index
+     * @return
+     */
+    public char remove( int index )
+    {
+        return '!';
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @param element
+     * @return
+     */
+    public boolean remove( char element )
+    {
+        return false;
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @return JAVADOC
+     */
+    public String toString()
+    {
+        return java.util.Arrays.toString( charArray );
+    }
+
+    /**
+     * JAVADOC
+     *
+     * @param arr
+     * @return
+     */
+    public boolean equals( Arrayz arr )
+    {
+        return false;
+    }
+
+    /**
+     * This method resets all of the elements of the charArray as:
+     *  "AP Compsi A Arrayz Lab", all as lowercase chars ignoring spaces
+     */
+    public void testingStarter()
+    {
+        charArray = new char[]{ 'a', 'p', 'c', 'o', 'm', 'p', 's', 'c', 'i', 'a',
+                                'a', 'r', 'r', 'a', 'y', 'z', 'l', 'a', 'b', ' ' };
+        numberOfElements = CAPACITY - 1;
+    }
+}
---
+++ src/Labs/Arrayz/Comparison.java
@@ -0,0 +1,34 @@
+package src.Labs.Arrayz;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * JAVADOC HERE
+ *
+ * @author Nate Johnson
+ * @version 11/15/24
+ */
+public class Comparison
+{
+    /*TODO:
+     * javadoc
+     * remove package header and main() parameters
+     */
+    private static ArrayList< Character > list;
+    private static Arrayz arrayz;
+
+    public static void main(String[] args)
+    {
+        list = new ArrayList< Character >();
+        arrayz = new Arrayz();
+
+        arrayz.testingStarter();
+        
+        System.out.println(arrayz.toString());
+        System.out.println(arrayz.get(5));
+
+        arrayz.clear();
+        System.out.println(arrayz.toString());
+    }
+}
