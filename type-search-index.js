f5ff834874d0671c8042ddccb7a582b03c41ffdb
N-JJ26 <nathanielj613@gmail.com>
2024-11-21 14:59:26 -0500

    javadoc?

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"src.Labs.Arrayz","l":"Arrayz"},{"p":"src.Labs.Arrayz","l":"Comparison"}];updateSearchResults();
