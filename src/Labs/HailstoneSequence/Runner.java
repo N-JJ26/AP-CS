ca3b2175377640f9cc51e38eed2166c922f319e3
N-JJ26 <nathanielj613@gmail.com>
2024-12-10 13:10:43 -0500

    trying to do the top 5

--- src/Labs/HailstoneSequence/Runner.java
+++ src/Labs/HailstoneSequence/Runner.java
@@ -1,7 +1,5 @@
 package src.Labs.HailstoneSequence;
 
-import java.util.ArrayList;
-
 /**
  * JAVADOC
  *
@@ -10,26 +8,26 @@ import java.util.ArrayList;
  */
 public class Runner {
     private static final int NUM_SEQUENCES = 1000;
-    private static final int TOP_FIVE = 5;
 
     private static Sequence sequence;
-    private static ArrayList<Sequence> sequences = new ArrayList<Sequence>();
+    private static Sequence[] sequences = new Sequence[NUM_SEQUENCES];
 
     private static int largestStone, longestSequence = 0;
 
-    private static int[] seeds = new int[TOP_FIVE];
+    private static int[] largest = new int[5];
+    private static int[] longest = new int[5];
 
     public static void main(String[] args) {
         for(int i = 1; i < NUM_SEQUENCES - 1; i++) {
             sequence = new Sequence(i);
-            sequences.add(sequence);
+            sequences[i - 1] = sequence;
 
             for(Sequence sequence : sequences) {
                 sequence = new Sequence(i);
 
                 if(sequence.getLargestStone() > largestStone) {
                     largestStone = sequence.getLargestStone();
-                    seeds[0] = sequence.getSeed();
+                    largest[0] = sequence.getSeed();
                 }
             }
 
@@ -38,14 +36,14 @@ public class Runner {
 
                 if(sequence.getLength() > longestSequence) {
                     longestSequence = sequence.getLength();
-                    seeds[1] = sequence.getSeed();
+                    longest[1] = sequence.getSeed();
                 }
             }
         }
 
         System.out.printf("\nseed = %d; stone = %d\n",
-                            seeds[0], largestStone);
+                            largest[0], largestStone);
         System.out.printf("\nseed = %d; length = %d\n",
-                                seeds[1], longestSequence);
+                            longest[1], longestSequence);
     }
 }
