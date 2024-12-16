e80d7bdc6f5dd4cc61d418e0097bcab8e346437c
N-JJ26 <nathanielj613@gmail.com>
2024-12-16 10:46:41 -0500

    IT WORKS

--- src/Labs/HailstoneSequence/Runner.java
+++ src/Labs/HailstoneSequence/Runner.java
@@ -7,43 +7,86 @@ package src.Labs.HailstoneSequence;
  * @version 12/10/24
  */
 public class Runner {
-    private static final int NUM_SEQUENCES = 1000;
+    private static final int NUM_SEQUENCES = 100000;
 
-    private static Sequence sequence;
     private static Sequence[] sequences = new Sequence[NUM_SEQUENCES];
 
-    private static int largestStone, longestSequence = 0;
+    private static Sequence[] longest, largest;
 
-    private static int[] largest = new int[5];
-    private static int[] longest = new int[5];
+    public static void main() {
+        longest = new Sequence[5];
+        largest = new Sequence[5];
 
-    public static void main(String[] args) {
-        for(int i = 1; i < NUM_SEQUENCES - 1; i++) {
-            sequence = new Sequence(i);
-            sequences[i - 1] = sequence;
+        for(int i = 0; i < NUM_SEQUENCES; i++)
+            sequences[i] = new Sequence(i + 1);
 
-            for(Sequence sequence : sequences) {
-                sequence = new Sequence(i);
+        for(int i = 0; i < 5; i++) {
+            longest[i] = sequences[i];
+            largest[i] = sequences[i];
+        }
 
-                if(sequence.getLargestStone() > largestStone) {
-                    largestStone = sequence.getLargestStone();
-                    largest[0] = sequence.getSeed();
-                }
+        longest = sortLongest(longest);
+        largest = sortLargest(largest);
+
+        for(Sequence sequence : sequences) {
+            if(sequence.getLength() > longest[0].getLength()) {
+                longest[0] = sequence;
+                longest = sortLongest(longest);
             }
 
-            for(Sequence sequence : sequences) {
-                sequence = new Sequence(i);
-
-                if(sequence.getLength() > longestSequence) {
-                    longestSequence = sequence.getLength();
-                    longest[1] = sequence.getSeed();
-                }
+            if(sequence.getLargestStone() > largest[0].getLargestStone()) {
+                largest[0] = sequence;
+                largest = sortLargest(largest);
             }
         }
 
-        System.out.printf("\nseed = %d; stone = %d\n",
-                            largest[0], largestStone);
-        System.out.printf("\nseed = %d; length = %d\n",
-                            longest[1], longestSequence);
+        for(int i = 4; i >= 0; i--) {
+            System.out.printf("seed: %d  stone: %d\n", largest[i].getSeed(), largest[i].getLargestStone());
+        }
+        System.out.println();
+        for(int i = 4; i >= 0; i--) {
+            System.out.printf("seed: %d  longest: %d\n", longest[i].getSeed(), longest[i].getLength());
+        }
+    }
+
+    private static Sequence[] sortLongest(Sequence[] arr) {
+        for(int i = 1; i < arr.length - 1; i++) {
+            boolean changed = false;
+            for(int j = 0; j < arr.length - i; j++) {
+                if(arr[j].getLength() > arr[j + 1].getLength()) {
+                    Sequence temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    changed = true;
+                }
+            }
+            if(!changed) {
+                break;
+            }
+        }
+        return arr;
+    }
+
+    private static Sequence[] sortLargest(Sequence[] arr) {
+        for(int i = 1; i < arr.length - 1; i++) {
+            boolean changed = false;
+            for(int j = 0; j < arr.length - i; j++) {
+                if(arr[j].getLargestStone() > arr[j + 1].getLargestStone()) {
+                    Sequence temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    changed = true;
+                }
+            }
+            if(!changed) {
+                break;
+            }
+        }
+        return arr;
+    }
+
+    private static boolean isFibonacci(int stone) {
+
+        return false;
     }
 }
