00ab498daa7f8faf940ed1748a5cfc8ff42eb647
N-JJ26 <nathanielj613@gmail.com>
2025-01-10 12:03:38 -0500

    removed the javadoc stuff

--- pages/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"src.Labs.Arrayz","l":"Arrayz"},{"p":"src.Labs.Arrayz","l":"Comparison"}];updateSearchResults();
