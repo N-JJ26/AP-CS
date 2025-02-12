a94d886ad259e3db071cc2b044f0fd1175c8ee5d
Avi Dasgupta <dasgupta.avi@nicholsschool.org>
2025-02-12 11:43:16 -0500

    Update Quadratic.java
    Almsot done with jacafocolsqwWDQ[2;'vc nd

--- src/Labs/thisIsSuperFUNctions/Quadratic.java
+++ src/Labs/thisIsSuperFUNctions/Quadratic.java
@@ -1,7 +1,20 @@
 package src.Labs.thisIsSuperFUNctions;
 
+/**
+* Quadratic Polynomial Function in standard form a*x^2 + b*x + c
+*
+* @author Avi D, Nate J.
+* @version February 12, 2025
+*/
 public class Quadratic extends Polynomial
 {
+   /**
+    * Constructs a Quadratic Polynomial Function of the form a*x^2 + b*x + c
+    *
+    * @param a the coefficient of the squared-term ( !=0 )
+    * @param b the coefficient of the linear term
+    * @param c the constant
+    */
     public Quadratic( double a, double b, double c )
     {
         super( new double[]{ c, b, a } );
