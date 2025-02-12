071cf8df3957ac42f45240384b1d1816bafd3528
Avi Dasgupta <dasgupta.avi@nicholsschool.org>
2025-02-12 11:52:03 -0500

    Update Quadratic.java
    almost...

--- src/Labs/thisIsSuperFUNctions/Quadratic.java
+++ src/Labs/thisIsSuperFUNctions/Quadratic.java
@@ -20,17 +20,32 @@ public class Quadratic extends Polynomial
         super( new double[]{ c, b, a } );
     }
 
+   /**
+    * Constructs a Quadratic Polynomial Function of the form a*x^2 + b*x + c
+    *
+    */
     public Quadratic()
     {
         this(1, 0, 0);
     }
 
+   /**
+    * Constructs a Quadratic Polynomial Function of the form a*x^2 + b*x + c
+    *
+    * @return 
+    */
     public Point vertex()
     {
         double xValue = Calc.derivative( this ).xIntercepts()[ 0 ].getX();
         return new Point( xValue, this.fOf(xValue) );
     }
 
+   /**
+    * Constructs a Quadratic Polynomial Function of the form a*x^2 + b*x + c
+    *
+    * @param a the coefficient of the squared-term ( !=0 )
+    * @return c the constant
+    */
     public Point[] xIntercepts( boolean allowComplex )
     {
         return this.xIntercepts();
