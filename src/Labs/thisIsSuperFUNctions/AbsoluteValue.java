e2e23b317149bda36206958a3ae135630d9cca5f
N-JJ26 <nathanielj613@gmail.com>
2025-02-12 14:51:28 -0500

    absolutevalue.java

---
+++ src/Labs/thisIsSuperFUNctions/AbsoluteValue.java
@@ -0,0 +1,13 @@
+package src.Labs.thisIsSuperFUNctions;
+
+public class AbsoluteValue extends Function
+{
+    private double a;
+    private Point vertex;
+
+    public AbsoluteValue( double a, Point vertex )
+    {
+        this.a = a;
+        this.vertex = vertex;
+    }
+}
