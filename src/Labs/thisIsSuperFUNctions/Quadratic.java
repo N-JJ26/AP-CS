3b878361b35b0911f82f4849c447b5cb5acdf4ea
Avi Dasgupta <dasgupta.avi@nicholsschool.org>
2025-02-12 14:52:10 -0500

    Update Quadratic.java
    rwefwefwe

--- src/Labs/thisIsSuperFUNctions/Quadratic.java
+++ src/Labs/thisIsSuperFUNctions/Quadratic.java
@@ -1,53 +1,54 @@
 package src.Labs.thisIsSuperFUNctions;
 
 /**
-* Quadratic Polynomial Function in standard form a*x^2 + b*x + c
-*
-* @author Avi D, Nate J.
-* @version February 12, 2025
-*/
+ * Quadratic Polynomial Function in standard form a*x^2 + b*x + c
+ *
+ * @author Nate J, Avi D.
+ * @version February 12, 2025
+ */
 public class Quadratic extends Polynomial
 {
-   /**
-    * Constructs a Quadratic Polynomial Function of the form a*x^2 + b*x + c
-    *
-    * @param a the coefficient of the squared-term ( !=0 )
-    * @param b the coefficient of the linear term
-    * @param c the constant
-    */
+    /**
+     * Constructs a Quadratic Polynomial Function of the form a*x^2 + b*x + c
+     *
+     * @param a the coefficient of the squared te4rm ( !=0 )
+     * @param b the coefficient of the linear-term
+     * @param c the constant
+     */
     public Quadratic( double a, double b, double c )
     {
-        super( new double[]{ c, b, a } );
+        super( new double[]{ a, b, c } );
     }
 
-   /**
-    * Constructs a Quadratic Polynomial Function of the form a*x^2 + b*x + c
-    *
-    */
+    /**
+     * Default constructor for y = x^2
+     */
     public Quadratic()
     {
         this(1, 0, 0);
     }
 
-   /**
-    * Constructs a Quadratic Polynomial Function of the form a*x^2 + b*x + c
-    *
-    * @return 
-    */
+    /**
+     * Returns the vertex of the Quadratic
+     *
+     * @return vertex = ( -b/2a, f(-b/2a) )
+     */
     public Point vertex()
     {
-        double xValue = Calc.derivative( this ).xIntercepts()[ 0 ].getX();
-        return new Point( xValue, this.fOf(xValue) );
+        return null;
     }
 
-   /**
-    * Constructs a Quadratic Polynomial Function of the form a*x^2 + b*x + c
-    *
-    * @param a the coefficient of the squared-term ( !=0 )
-    * @return c the constant
-    */
+    /**
+     * Returns the Point[] = { ( x1, 0 ), ( x2, 0 ) }, or the complex conjugates
+     *  as Point objects where the x = real-part and y = imaginary-part
+     *  i.e. 2 +/- 5i => { ( 2, 5 ), ( 2, -5 ) }
+     *
+     * @param allowCompex true if complex x-its are allowed, false otherwise
+     * @return the single x-int, two real x-ints, or
+     *  two complex x-ints if complex numbers are allowed.
+     */
     public Point[] xIntercepts( boolean allowComplex )
     {
-        return this.xIntercepts();
+        return null;
     }
 }
