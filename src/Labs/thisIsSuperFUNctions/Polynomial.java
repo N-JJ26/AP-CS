2f59f3857ff1035c40e0dcf3d81b19f5b69ecb4a
N-JJ26 <nathanielj613@gmail.com>
2025-02-26 08:57:45 -0500

    huh?

--- src/Labs/thisIsSuperFUNctions/Polynomial.java
+++ src/Labs/thisIsSuperFUNctions/Polynomial.java
@@ -44,10 +44,7 @@ public class Polynomial extends Function
 
         a = newCoeff;
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> 377179de911ec62089a1b296417d9b0fd34b2d18
     /**
      * Compares the average value of this Polynomial to an
      *  other Polynomial over a given closed interval [ a, b ], where a <= b
